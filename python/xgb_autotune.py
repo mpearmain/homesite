63b0594d67dc205584346a74bd0df466f7c2d22a
mpearmain <michael.pearmain@gmail.com>
2015-11-12 08:13:35 +0000

    amending optimization params

--- python/xgb_autotune.py
+++ python/xgb_autotune.py
@@ -13,7 +13,7 @@ def xgboostcv(max_depth,
               n_estimators,
               subsample,
               colsample_bytree,
-              silent=True,
+              silent=False,
               nthread=-1,
               seed=1234):
 
@@ -67,34 +67,31 @@ if __name__ == "__main__":
     print('Final Results')
     print('XGBOOST: %f' % xgboostBO.res['max']['max_val'])
 
-
-    # Build and Run on the full data set K-fold times for bagging
-
-    pred_average = True
-    seeds = [1234, 2606081, 998466, 229883, 8838120]
-    for seed_bag in seeds:
-        clf = XGBClassifier(max_depth=int(xgboostBO.res['max']['max_params']['max_depth']),
-                                               learning_rate=xgboostBO.res['max']['max_params']['learning_rate'],
-                                               n_estimators=int(xgboostBO.res['max']['max_params']['n_estimators']),
-                                               gamma=xgboostBO.res['max']['max_params']['gamma'],
-                                               min_child_weight=xgboostBO.res['max']['max_params']['min_child_weight'],
-                                               max_delta_step=xgboostBO.res['max']['max_params']['max_delta_step'],
-                                               subsample=xgboostBO.res['max']['max_params']['subsample'],
-                                               colsample_bytree=xgboostBO.res['max']['max_params']['colsample_bytree'],
-                                               seed=seed_bag,
-                                               objective="binary:logistic")
-
-        clf.fit(x_train, y_train, eval_metric="auc", eval_set=[(x_valid, y_valid)], early_stopping_rounds=25)
-        print('Prediction Complete')
-        preds = clf.predict_proba(test)[:, 1]
-        if type(pred_average) == bool:
-            pred_average = preds.copy()/len(seeds)
-        else:
-            pred_average += preds/len(seeds)
-
-        submission = pd.read_csv('input/sample_submission.csv')
-        submission.QuoteConversion_Flag = pred_average
-        outfile_seed = 'output/xgb_v2_bag_' + len(seed_bag) + '.csv'
-        submission.to_csv(outfile_seed)
-
-
+    #
+    # # Build and Run on the full data set K-fold times for bagging
+    #
+    # pred_average = True
+    # seeds = [1234, 2606081, 998466, 229883, 8838120]
+    # for seed_bag in seeds:
+    #     clf = XGBClassifier(max_depth=int(xgboostBO.res['max']['max_params']['max_depth']),
+    #                         learning_rate=xgboostBO.res['max']['max_params']['learning_rate'],
+    #                         n_estimators=int(xgboostBO.res['max']['max_params']['n_estimators']),
+    #                         subsample=xgboostBO.res['max']['max_params']['subsample'],
+    #                         colsample_bytree=xgboostBO.res['max']['max_params']['colsample_bytree'],
+    #                         seed=seed_bag,
+    #                         objective="binary:logistic")
+    #
+    #     clf.fit(x_train, y_train, eval_metric="auc", eval_set=[(x_valid, y_valid)], early_stopping_rounds=25)
+    #     print('Prediction Complete')
+    #     preds = clf.predict_proba(test)[:, 1]
+    #     if type(pred_average) == bool:
+    #         pred_average = preds.copy()/len(seeds)
+    #     else:
+    #         pred_average += preds/len(seeds)
+    #
+    #     submission = pd.read_csv('input/sample_submission.csv')
+    #     submission.QuoteConversion_Flag = pred_average
+    #     outfile_seed = 'output/xgb_v2_bag_' + len(seed_bag) + '.csv'
+    #     submission.to_csv(outfile_seed)
+    #
+    #
