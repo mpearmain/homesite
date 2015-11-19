b7af46768d48ce722bf839fe1a8aa7cf70a88dc9
mpearmain <michael.pearmain@gmail.com>
2015-11-19 10:35:47 +0000

    minor fixes to names

--- R/folds_prep.R
+++ R/folds_prep.R
@@ -14,9 +14,10 @@ msg <- function(mmm,...)
 
 ## create folds ####
 # load data
+
 xtrain <- read_csv(file = "./input/train.csv")
-id_train <- xtrain$ID; xtrain$ID <- NULL
-y <- xtrain$target; xtrain$target <- NULL
+id_train <- xtrain$QuoteNumber; xtrain$QuoteNumber <- NULL
+y <- xtrain$QuoteConversion_Flag; xtrain$QuoteConversion_Flag <- NULL
 
 
 set.seed(20150817)
@@ -31,14 +32,12 @@ xfolds <- array(0, c(length(y), 4))
 xfolds[,1] <- id_train
 # column 2 = 5-fold split, where each row gets the {1,2,3,4,5} id assigned
 # this is R counting convention => starts from 1!
-for (ii in seq(idFix5))
-{
+for (ii in seq(idFix5)) {
   xfolds[ idFix5[[ii]],2] <- ii
 }
 # column 3 = 10-fold split, where each row gets the {1,...,10} id assigned
 # this is R counting convention => starts from 1!
-for (ii in seq(idFix10))
-{
+for (ii in seq(idFix10)) {
   xfolds[ idFix10[[ii]],3] <- ii
 }
 # column 4 = train/valid split (0 = train, 1 = valid)
@@ -46,5 +45,5 @@ xfolds[subrange,4] <- 1
 
 # store 
 xfolds <- data.frame(xfolds)
-colnames(xfolds) <- c("Id", "fold5", "fold10", "valid")
+colnames(xfolds) <- c("QuoteNumber", "fold5", "fold10", "valid")
 write_csv(xfolds, path = "./input/xfolds.csv")
