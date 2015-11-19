49adc9f9209ee0018317fdb9ee25fa8d6076219e
mpearmain <michael.pearmain@gmail.com>
2015-11-19 10:25:19 +0000

    moving binary hashing into R folder -- Where it belongs

--- R/binary_data_xgb.R
+++ R/binary_data_xgb.R
@@ -2,16 +2,18 @@
 library(data.table)
 library(FeatureHashing)
 library(xgboost)
+library(Matrix)
 
 train_full <- fread('input/xtrain_full.csv')
 train <- fread('input/xtrain.csv')
 valid <- fread('input/xvalid.csv')
 test <- fread('input/xtest.csv')
-
+submission <- fread('input/sample_submission.csv')
 
 train_full[, QuoteNumber := NULL]
 train[, QuoteNumber := NULL]
 valid[, QuoteNumber := NULL]
+test_quote <- test[, QuoteNumber, with=F]
 test[, QuoteNumber := NULL]
 
 # Carve out things like log dates and salesField 28 and 'join' to the 
@@ -53,24 +55,40 @@ hash_train <- hashed.model.matrix(~., train, 2^16)
 hash_valid <- hashed.model.matrix(~., valid, 2^16)
 hash_test <- hashed.model.matrix(~., test, 2^16)
 
+# Now append the numeric cols to the hashed objects for better usage of numerics.
+# Need to convert into sparse data format first in order to join the matrix objects.
+numeric_sparse_train_full <- Matrix(as.matrix(numeric_mat_train_full), sparse = TRUE)
+numeric_sparse_train <- Matrix(as.matrix(numeric_mat_train), sparse = TRUE)
+numeric_sparse_valid <- Matrix(as.matrix(numeric_mat_valid), sparse = TRUE)
+numeric_sparse_test <- Matrix(as.matrix(numeric_mat_test), sparse = TRUE)
+
 # Should export data here -> useful for NN as wellas bins
+hash_full <- cbind(hash_full, numeric_sparse_train_full)
+hash_train <- cbind(hash_train, numeric_sparse_train)
+hash_valid <- cbind(hash_valid, numeric_sparse_valid)
+hash_test <- cbind(hash_test, numeric_sparse_test)
 
 dtrain <- xgb.DMatrix(hash_train, label = y_train)
 dvalid <- xgb.DMatrix(hash_valid, label = y_valid)
 watch <- list(valid = dvalid, train = dtrain)
 
-m1 <- xgb.train(booster = "gbtree",
-                maximize = TRUE, 
-                print.every.n = 25,
-                early.stop.round = 50,
-                nrounds = 2000,
-                eta = 0.028,
-                max.depth = 50, 
-                colsample_bytree = 0.9,
-                subsample = 0.85,
-                data = dtrain, 
-                objective = "binary:logistic",
-                watchlist = watch, 
-                eval_metric = "auc")
+clf <- xgb.train(booster = "gbtree",
+                 maximize = TRUE, 
+                 print.every.n = 25,
+                 early.stop.round = 50,
+                 nrounds = 3000,
+                 eta = 0.03,
+                 max.depth = 30, 
+                 colsample_bytree = 0.85,
+                 subsample = 0.8,
+                 data = dtrain, 
+                 objective = "binary:logistic",
+                 watchlist = watch, 
+                 eval_metric = "auc")
+
+pred <- predict(clf, hash_test)
+
+submission[, QuoteConversion_Flag := pred]
+write.csv(submission, 'output/xgb_binary.csv', row.names = FALSE)
 
 
