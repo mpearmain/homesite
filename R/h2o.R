2182e7a73668cf3bb059d3136c0c0ff81ed0d2c7
tng-konrad <konrad@Konrads-MacBook-Pro.local>
2015-11-26 16:34:55 +0100

    corrected dl on v5

--- R/h2o.R
+++ R/h2o.R
@@ -26,8 +26,8 @@ auc<-function (actual, predicted) {
 
 ## data ####
 # read
-xtrain <- read_csv("./input/xtrain_kb4.csv")
-xtest <- read_csv("./input/xtest_kb4.csv")
+xtrain <- read_csv("./input/xtrain_kb5.csv")
+xtest <- read_csv("./input/xtest_kb5.csv")
 y <- xtrain$QuoteConversion_Flag; xtrain$QuoteConversion_Flag <- NULL
 xtrain$QuoteNumber <- xtest$QuoteNumber <- NULL
 xtrain$SalesField8 <- xtest$SalesField8 <- NULL
@@ -40,43 +40,19 @@ xtrain$target <- factor(y)
 
 xvalid <- xtrain[isValid,]; xtrain <- xtrain[isTrain,]
 
-size1 <- ncol(xtrain); size2 <- round(0.5 * size1)
+size1 <- ncol(xtrain)-1; size2 <- round(0.5 * size1)
 xrange <- 1:size1
 
 train.hex <- as.h2o(xtrain); valid.hex <- as.h2o(xvalid)
 xseed <- 123; test.hex <- as.h2o(xtest)
 
-# compare performance of different activations
-activ_list <- c("Rectifier", "Tanh", "TanhWithDropout",
-                               "RectifierWithDropout", "Maxout", "MaxoutWithDropout")
-activ_auc <- rep(0, length(activ_list))
-for (ii in seq(activ_list))
-{
-  dl.model <- h2o.deeplearning(
-    # data specifications
-    x = xrange, y = ncol(xtrain), training_frame = train.hex, 
-    autoencoder = FALSE, 
-    # network structure: activation and geometry
-    activation = activ_list[ii],
-    hidden = c(size1, size2), epochs = 20, 
-    input_dropout_ratio = 0.05, hidden_dropout_ratios = c(0.05, 0.02), 
-    # parameters of the optimization process
-    rho = 0.99, epsilon = 1e-08, rate = 0.005,
-    rate_annealing = 1e-06, rate_decay = 1, momentum_start = 0.5,
-    
-    l1 = 0, l2 = 0,  loss = c("CrossEntropy")
-  )
-  
-  pred_valid <- as.data.frame(predict(dl.model, valid.hex))$p1
-  pred_full <- as.data.frame(predict(dl.model, test.hex))$p1
-  activ_auc[ii] <- auc(y[isValid], pred_valid)
-  
-}
+rm(xtrain, xtest, xvalid)
 
 ## generate prediction ####
+# build a dl.model using rectifierWithDropout activation
 dl.model <- h2o.deeplearning(
   # data specifications
-  x = xrange, y = ncol(xtrain), training_frame = train.hex, 
+  x = xrange, y = max(xrange)+1, training_frame = train.hex, 
   autoencoder = FALSE, 
   # network structure: activation and geometry
   activation = "RectifierWithDropout",
@@ -96,5 +72,5 @@ pred1 <- pred_full
 x <- read_csv("./input/xtest_kb4.csv")
 xsub <- data.frame(QuoteNumber = x$QuoteNumber, QuoteConversion_Flag = pred1)
 xsub$QuoteConversion_Flag <- rank(pred1)/nrow(xsub)
-write_csv(xsub, "./submissions/h2o_kb4_v4.csv")
+write_csv(xsub, "./submissions/h2o_kb5_v1.csv")
 
