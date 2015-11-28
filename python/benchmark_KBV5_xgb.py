05e31a5a6e1320d20a36f087f438bbf394580608
mpearmain <michael.pearmain@gmail.com>
2015-11-28 08:23:56 +0000

    Adding benchmark for kbv5 and mpv2

--- python/benchmark_KBV5_xgb.py
+++ python/benchmark_KBV5_xgb.py
@@ -8,9 +8,9 @@ import pandas as pd
 import xgboost as xgb
 
 print('Loading Train data set')
-x_train = pd.read_csv('input/xtrain_mp2.csv')
+x_train = pd.read_csv('input/xtrain_kb5.csv')
 print('Loading Test data set')
-test = pd.read_csv('input/xtest_mp2.csv')
+test = pd.read_csv('input/xtest_kb5.csv')
 
 sample = pd.read_csv('input/sample_submission.csv')
 
@@ -22,16 +22,17 @@ x_train = x_train.fillna(-1)
 test = test.fillna(-1)
 
 pred_average = True
-no_bags = 10
+no_bags = 2
 for k in range(no_bags):
     print 'Building bag:', k
-    clf = xgb.XGBClassifier(n_estimators=2000,
+    clf = xgb.XGBClassifier(n_estimators=3940,
                             nthread=-1,
-                            max_depth=7,
-                            learning_rate=0.02,
+                            max_depth=17,
+                            learning_rate=0.01652,
                             silent=True,
-                            subsample=0.86,
-                            colsample_bytree=0.68,
+                            subsample=0.7247,
+                            colsample_bytree=0.56303,
+                            max_delta_step=0.1002898543,
                             seed=k*100+22)
     xgb_model = clf.fit(x_train, y_train, eval_metric="auc")
     preds = clf.predict_proba(test)[:,1]
@@ -41,4 +42,4 @@ for k in range(no_bags):
         pred_average += preds/no_bags
 
 sample.QuoteConversion_Flag = pred_average
-sample.to_csv('output/xgb_homesite_10bag_mpv2_26112015.csv', index=False)
+sample.to_csv('output/xgb_homesite_2bag_kb5_28112015.csv', index=False)
