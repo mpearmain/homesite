217673e1870b3450f6c2ac00d22ec7c528025b28
mpearmain <michael.pearmain@gmail.com>
2016-01-03 11:37:00 +0000

    Creating mp3 changing xgboost meta to run for mp3

--- R/data_preparation.R
+++ R/data_preparation.R
@@ -180,6 +180,111 @@ BuildMP2 <- function() {
 
 BuildMP2()
 
+BuildMP3 <- function() {
+  # Wrapping the builds in functions to make it easier to call just one Dataset Build.
+  # Working Dir should be top level with folders ./R, ./input, ./output
+  train <- fread('input/train.csv')
+  test <- fread('input/test.csv')
+  
+  # Lets first align the datasets for equal vars to work with.
+  y <- train[, QuoteConversion_Flag]
+  train[, c('QuoteConversion_Flag') := NULL]
+  train[, dset := 0]
+  test[, dset := 1]
+  
+  #Quick check to align data dimensions.
+  stopifnot(dim(test)[2] ==dim(train)[2])
+  # Join the datasets for simple manipulations.
+  bigD <- rbind(train, test)
+  rm(list = c('train', 'test'))
+  
+  ## Data Manipulations 
+  # Lets solve the Field10 issue: 1,165 => 1165.
+  bigD[, Field10 := as.numeric(gsub(",", "", Field10))]
+  
+  # First lets work with the dates and remove "Original_Quote_Date" field.
+  bigD[, Date := parse_date_time(Original_Quote_Date, "%Y%m%d")]
+  bigD[, Original_Quote_Date := NULL]
+  
+  # Lets get the year, month, day of month, weekday,
+  bigD[, year := as.factor(year(Date))]
+  bigD[, month := as.factor(month(Date))]
+  bigD[, monthday := as.factor(mday(Date))]
+  bigD[, weekday := as.factor(wday(Date))]
+  # Adding year day - people rebuy the same year?
+  bigD[, yearday := as.factor(yday(Date))]
+  
+  # Fake duration date - How long since the start of the comp (Mon 9 Nov 2015)
+  # Taking logs as magnitude is an order difference 175 min 1042 max
+  # Checking distributions between train and test proves equal distributions. --> Good to know for CV.
+  bigD[, daysDurOrigQuote := as.integer(parse_date_time("2015-11-09", "%Y%m%d") - Date)]
+  bigD[, logDaysDurOrigQuote := log(as.integer(parse_date_time("2015-11-09", "%Y%m%d") - Date))]
+  bigD[, Date := NULL]
+  
+  # Lets model temporal effects in time
+  # http://www.rochester.edu/College/PSC/signorino/research/Carter_Signorino_2010_PA.pdf
+  # Needs more work for anything useful
+  #bigD[, logdaysDurOrigQuote2 := log((daysDurOrigQuote)^2 - daysDurOrigQuote)]
+  #bigD[, logdaysDurOrigQuote3 := log((daysDurOrigQuote)^3 - daysDurOrigQuote)]
+  
+  bigD[is.na(bigD)] <- -1
+  
+  # Count -1's across the data set
+  bigD[, CoverageNeg1s := rowSums(.SD == -1), .SDcols = grep("CoverageField", names(bigD))]
+  bigD[, SalesNeg1s := rowSums(.SD == -1), .SDcols = grep("SalesField", names(bigD))]
+  bigD[, PropertyNeg1s := rowSums(.SD == -1), .SDcols = grep("PropertyField", names(bigD))]
+  bigD[, GeoNeg1s := rowSums(.SD == -1), .SDcols = grep("GeographicField", names(bigD))]
+  bigD[, PersonalNeg1s := rowSums(.SD == -1), .SDcols = grep("PersonalField", names(bigD))]
+  # Finally Total across all.
+  bigD[,
+       TotalNeg1s := rowSums(.SD),
+       .SDcols = c("CoverageNeg1s", "SalesNeg1s", "PropertyNeg1s", "GeoNeg1s", "PersonalNeg1s")]
+  
+  # Count 0's across the data set
+  bigD[, Coverage0s := rowSums(.SD == 0), .SDcols = grep("CoverageField", names(bigD))]
+  bigD[, Sales0s := rowSums(.SD == 0), .SDcols = grep("SalesField", names(bigD))]
+  bigD[, Property0s := rowSums(.SD == 0), .SDcols = grep("PropertyField", names(bigD))]
+  bigD[, Geo0s := rowSums(.SD == 0), .SDcols = grep("GeographicField", names(bigD))]
+  bigD[, Personal0s := rowSums(.SD == 0), .SDcols = grep("PersonalField", names(bigD))]
+  # Finally Total across all.
+  bigD[,
+       Total0s := rowSums(.SD),
+       .SDcols = c("Coverage0s", "Sales0s", "Property0s", "Geo0s", "Personal0s")]
+  
+  # Catch factor columns
+  fact_cols <- which(lapply(bigD, class) == "character")
+  # Map all categoricals into numeric.
+  cat("Assuming text variables are categorical & replacing them with numeric ids\n")
+  for (f in colnames(bigD)) {
+    if (class(bigD[[f]])=="character") {
+      print(f)
+      levels <- unique(bigD[[f]])
+      bigD[[f]] <- as.integer(factor(bigD[[f]], levels=levels))
+    }
+  }
+  
+  ## Split files & Export 
+  xtrain <- bigD[dset == 0, ]
+  xtest <- bigD[dset == 1, ]
+  rm(bigD)
+  
+  xtrain[, dset := NULL]
+  xtest[, dset := NULL]
+  
+  xtrain[, QuoteConversion_Flag := y]
+  
+  write.csv(xtrain, 'input/xtrain_mp3.csv', row.names = F)
+  write.csv(xtest, 'input/xtest_mp3.csv', row.names = F)
+  
+  rm(xtrain)
+  rm(xtest)
+  return(cat("MP1 dataset built"))
+}
+
+BuildMP3()
+
+
+
 ## KB set v1: factors mapped to integers ####
 # map everything to integers
 # read
--- python/build_meta_xgboost.py
+++ python/build_meta_xgboost.py
@@ -15,32 +15,32 @@ if __name__ == '__main__':
 
     ## settings
     projPath = './'
-    dataset_version = "mp1"
-    model_type = "xgb" 
+    dataset_version = "mp3"
+    model_type = "xgb"
     seed_value = 123
     todate = datetime.datetime.now().strftime("%Y%m%d")
-    	    
+
     ## data
     # read the training and test sets
-    xtrain = pd.read_csv(projPath + 'input/xtrain_'+ dataset_version + '.csv')     
+    xtrain = pd.read_csv(projPath + 'input/xtrain_'+ dataset_version + '.csv')
     id_train = xtrain.QuoteNumber
     ytrain = xtrain.QuoteConversion_Flag
     xtrain.drop('QuoteNumber', axis = 1, inplace = True)
     xtrain.drop('QuoteConversion_Flag', axis = 1, inplace = True)
-    
-    xtest = pd.read_csv(projPath + 'input/xtest_'+ dataset_version + '.csv')     
+
+    xtest = pd.read_csv(projPath + 'input/xtest_'+ dataset_version + '.csv')
     id_test = xtest.QuoteNumber
     xtest.drop('QuoteNumber', axis = 1, inplace = True)
-        
+
     # folds
     xfolds = pd.read_csv(projPath + 'input/xfolds.csv')
     # work with 5-fold split
     fold_index = xfolds.fold5
     fold_index = np.array(fold_index) - 1
     n_folds = len(np.unique(fold_index))
-    
+
     ## model
-    # parameter grids: LR + range of training subjects to subset to 
+    # parameter grids: LR + range of training subjects to subset to
     '''
     Staying with index convention.
         child_weight = 0
@@ -52,12 +52,13 @@ if __name__ == '__main__':
         ntrees = 6
     '''
     param_grid = [
+        (10, 4, 0.93, 0.856, 0.1, 0.02, 150),
         (1, 40, 0.73, 0.756, 0.001, 0.02, 1500),
         (4, 8, 0.87, 0.798, 0.0007, 0.043, 400),
         (10, 6, 0.60, 0.90, 0, 0.0432, 380),
         (3, 8, 0.87, 0.7890, 0, 0.09087, 350),
         (1, 3, 0.789, 0.97, 0, 0.012, 1100),
-        (1, 10, 0.97, 0.95, 0.0001, 0.0097, 6200),
+        (1, 7, 0.74, 0.64, 0.00015, 0.008, 6200),
         (7, 9, 0.53, 0.879, 0.005, 0.019, 900),
         (7, 6, 0.80, 0.54, 0, 0.0215, 1300),
         (7, 3, 0.50, 0.66, 0, 0.08732, 300),
@@ -74,56 +75,67 @@ if __name__ == '__main__':
         (9, 5, 0.7, 0.98032, 0, 0.0289, 900),
         (5, 9, 0.73, 0.7649, 0, 0.0187, 900)
     ]
+
     # storage structure for forecasts
     mvalid = np.zeros((xtrain.shape[0],len(param_grid)))
     mfull = np.zeros((xtest.shape[0],len(param_grid)))
-    
+
     ## build 2nd level forecasts
-    for i in range(len(param_grid)):        
-            print "processing parameter combo:", param_grid[i]
-            # configure model with j-th combo of parameters
-            x = param_grid[i]
-            clf = xgb.XGBClassifier(n_estimators=x[6],
-                                    nthread=-1,
-                                    max_depth=x[1],
-                                    min_child_weight=x[0],
-                                    learning_rate=x[5],
-                                    silent=True,
-                                    subsample=x[3],
-                                    colsample_bytree=x[2],
-                                    gamma=x[2],
-                                    seed=seed_value)
-            
-            # loop over folds - Keeping as pandas for ease of use with xgb wrapper
-            for j in range(1, n_folds+1):
-                print "Processing Fold", j
-                idx0 = xfolds[xfolds.fold5 != j].index
-                idx1 = xfolds[xfolds.fold5 == j].index
-                x0 = xtrain[xtrain.index.isin(idx0)]
-                x1 = xtrain[xtrain.index.isin(idx1)]
-                y0 = ytrain[ytrain.index.isin(idx0)]
-                y1 = ytrain[ytrain.index.isin(idx1)]
+    for i in range(len(param_grid)):
+        print "processing parameter combo:", param_grid[i]
+        # configure model with j-th combo of parameters
+        x = param_grid[i]
+        clf = xgb.XGBClassifier(n_estimators=x[6],
+                                nthread=-1,
+                                max_depth=x[1],
+                                min_child_weight=x[0],
+                                learning_rate=x[5],
+                                silent=True,
+                                subsample=x[3],
+                                colsample_bytree=x[2],
+                                gamma=x[2],
+                                seed=seed_value)
 
-                # fit the model on observations associated with subject whichSubject in this fold
-                clf.fit(x0, y0, eval_metric="auc")
-                mvalid[idx1,i] = clf.predict_proba(x1)[:,1]
+        # loop over folds - Keeping as pandas for ease of use with xgb wrapper
+        for j in range(1 ,n_folds+1):
+            idx0 = xfolds[xfolds.fold5 != j].index
+            idx1 = xfolds[xfolds.fold5 == j].index
+            x0 = xtrain[xtrain.index.isin(idx0)]
+            x1 = xtrain[xtrain.index.isin(idx1)]
+            y0 = ytrain[ytrain.index.isin(idx0)]
+            y1 = ytrain[ytrain.index.isin(idx1)]
 
-            # fit on complete dataset
-            clf.fit(xtrain, ytrain, eval_metric="auc")
-            mfull[:,i] = clf.predict_proba(xtest)[:,1]
-            
-        
-            ## store the results
-            # add indices etc
-            mvalid = pd.DataFrame(mvalid)
-            mvalid.columns = [model_type + str(k) for k in range(0, mvalid.shape[1])]
-            mvalid['QuoteNumber'] = id_train
-            mvalid['QuoteConversion_Flag'] = ytrain
-            
-            mfull = pd.DataFrame(mfull)
-            mfull.columns = [model_type + str(k) for k in range(0, mfull.shape[1])]
-            mfull['QuoteNumber'] = id_test
+            # fit the model on observations associated with subject whichSubject in this fold
+            clf.fit(x0, y0, eval_metric="auc", eval_set=[(x1, y1)])
+            mvalid[idx1,i] = clf.predict_proba(x1)[:,1]
 
-            # save the files
-            mvalid.to_csv(projPath + 'metafeatures/prval_' + model_type + '_' + todate + '_data' + dataset_version + '_seed' + str(seed_value) + '_param' + str(i) + '.csv', index = False, header = True)
-            mfull.to_csv(projPath + 'metafeatures/prfull_' + model_type + '_' + todate + '_data' + dataset_version + '_seed' + str(seed_value) + '_param' + str(i) + '.csv', index = False, header = True)
+        # fit on complete dataset
+        bst = xgb.XGBClassifier(n_estimators=x[6],
+                                nthread=-1,
+                                max_depth=x[1],
+                                min_child_weight=x[0],
+                                learning_rate=x[5],
+                                silent=True,
+                                subsample=x[3],
+                                colsample_bytree=x[2],
+                                gamma=x[2],
+                                seed=seed_value)
+        bst.fit(xtrain, ytrain, eval_metric="auc")
+        mfull[:,i] = bst.predict_proba(xtest)[:,1]
+
+
+    ## store the results
+    # add indices etc
+    mvalid = pd.DataFrame(mvalid)
+    mvalid.columns = [model_type + str(i) for i in range(0, mvalid.shape[1])]
+    mvalid['QuoteNumber'] = id_train
+    mvalid['QuoteConversion_Flag'] = ytrain
+
+    mfull = pd.DataFrame(mfull)
+    mfull.columns = [model_type + str(i) for i in range(0, mfull.shape[1])]
+    mfull['QuoteNumber'] = id_test
+
+
+    # save the files
+    mvalid.to_csv(projPath + 'metafeatures/prval_' + model_type + '_' + todate + '_data' + dataset_version + '_seed' + str(seed_value) + '.csv', index = False, header = True)
+    mfull.to_csv(projPath + 'metafeatures/prfull_' + model_type + '_' + todate + '_data' + dataset_version + '_seed' + str(seed_value) + '.csv', index = False, header = True)
