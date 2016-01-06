46b21ab5a351ca9c9578f8c8c73ee4cbcad2ed11
tng-konrad <konrad@Konrads-MacBook-Pro.local>
2016-01-06 08:10:50 +0100

    added hillclimb function

--- R/build_ensemble.R
+++ R/build_ensemble.R
@@ -27,6 +27,58 @@ auc<-function (actual, predicted) {
   
 }
 
+# build an ensemble, input = parameters(initSize,howMany,blendIt, blendProp),
+# input x, input y (x0 / y0 in c-v)
+# output = list(weight)
+buildEnsemble <- function(parVec, xset, yvec)
+{
+  set.seed(20130912)
+  # ensemble settings
+  initSize <- parVec[1]; howMany <- parVec[2];
+  blendIt <- parVec[3]; blendProp <- parVec[4]
+  
+  # storage matrix for blending coefficients
+  arMat <- array(0, c(blendIt, ncol(xset)))
+  colnames(arMat) <- colnames(xset)
+  
+  # loop over blending iterations
+  dataPart <- createDataPartition(1:ncol(arMat), times = blendIt, p  = blendProp)
+  for (bb in 1:blendIt)
+  {
+    idx <- dataPart[[bb]];    xx <- xset[,idx]
+    
+    # track individual scores
+    trackScore <- apply(xx, 2, function(x) auc(yvec,x))
+    
+    # select the individual best performer - store the performance
+    # and create the first column -> this way we have a non-empty ensemble
+    bestOne <- which.max(trackScore)
+    mastaz <- (rank(-trackScore) <= initSize)
+    best.track <- trackScore[mastaz];    hillNames <- names(best.track)
+    hill.df <- xx[,mastaz, drop = FALSE]
+    
+    # loop over adding consecutive predictors to the ensemble
+    for(ee in 1 : howMany)
+    {
+      # add a second component
+      trackScoreHill <- apply(xx, 2,
+                              function(x) auc(yvec,rowMeans(cbind(x , hill.df))))
+      
+      best <- which.max(trackScoreHill)
+      best.track <- c(best.track, max(trackScoreHill))
+      hillNames <- c(hillNames,names(best))
+      hill.df <- data.frame(hill.df, xx[,best])
+    }
+    
+    ww <- summary(factor(hillNames))
+    arMat[bb, names(ww)] <- ww
+  }
+  
+  wgt <- colSums(arMat)/sum(arMat)
+  
+  return(wgt)
+}
+
 ## data ####
 # list the groups 
 xlist_val <- dir("./metafeatures/", pattern = "prval", full.names = T)
@@ -90,7 +142,21 @@ xfolds <- xfolds[,c("QuoteNumber", "fold_index")]
 nfolds <- length(unique(xfolds$fold_index))
 
 
-storage_matrix <- array(0, c(nfolds, 6))
+storage_matrix <- array(0, c(nfolds, 8))
+
+xMed <- apply(xvalid,1,median)
+xMin <- apply(xvalid,1,min)
+xMax <- apply(xvalid,1,max)
+xvalid$xmed <- xMed
+xvalid$xmax <- xMax
+xvalid$xmin <- xMin
+
+xMed <- apply(xfull,1,median)
+xMin <- apply(xfull,1,min)
+xMax <- apply(xfull,1,max)
+xfull$xmed <- xMed
+xfull$xmax <- xMax
+xfull$xmin <- xMin
 
 for (ii in 1:nfolds)
 {
@@ -106,7 +172,14 @@ for (ii in 1:nfolds)
   prx1 <- prx[,ncol(prx)]
   
   storage_matrix[ii,1] <- auc(y1,prx1)
- 
+
+  mod0 <- glmnet(x = as.matrix(x0), y = y0, alpha = 1)
+  prx <- predict(mod0,as.matrix(x1))
+  prx2 <- prx[,ncol(prx)]
+  
+  storage_matrix[ii,2] <- auc(y1,prx2)
+  storage_matrix[ii,3] <- auc(y1,prx2 + prx1)
+  
   x0d <- xgb.DMatrix(as.matrix(x0), label = y0)
   x1d <- xgb.DMatrix(as.matrix(x1), label = y1)
   
@@ -114,7 +187,7 @@ for (ii in 1:nfolds)
   
   clf <- xgb.train(booster = "gbtree",
                    maximize = TRUE, 
-                   print.every.n = 25,
+                   print.every.n = 50,
                    # early.stop.round = 25,
                    nrounds = 250,
                    eta = 0.01,
@@ -127,15 +200,16 @@ for (ii in 1:nfolds)
                    eval_metric = "auc",
                    gamma= 0.05)
   
-  prx2 <- predict(clf, x1d)
-  storage_matrix[ii,2] <- auc(y1,prx2)
+  prx3 <- predict(clf, x1d)
+  storage_matrix[ii,3] <- auc(y1,prx3)
   
-  storage_matrix[ii,3] <- auc(y1, prx1 + prx2)
-  storage_matrix[ii,4] <- auc(y1, rank(prx1) + rank(prx2))
+  storage_matrix[ii,4] <- auc(y1, prx1 + prx2)
+  storage_matrix[ii,5] <- auc(y1, prx1 + prx3)
+  storage_matrix[ii,6] <- auc(y1, 0.5 * (prx1 + prx2) + prx3)
   
   a <- apply(x1,2,function(s) auc(y1,s))
-  storage_matrix[ii,5] <- max(a)
-  storage_matrix[ii,6] <- which.max(a)
+  storage_matrix[ii,7] <- max(a)
+  storage_matrix[ii,8] <- which.max(a)
 }
 
 ## build final prediction
@@ -144,12 +218,16 @@ mod0 <- glmnet(x = as.matrix(xvalid), y = y, alpha = 0)
 prx <- predict(mod0,as.matrix(xfull))
 prx1 <- prx[,ncol(prx)]
 
+mod0 <- glmnet(x = as.matrix(xvalid), y = y, alpha = 1)
+prx <- predict(mod0,as.matrix(xfull))
+prx2 <- prx[,ncol(prx)]
+
 x0d <- xgb.DMatrix(as.matrix(xvalid), label = y)
 x1d <- xgb.DMatrix(as.matrix(xfull))
 
 clf <- xgb.train(booster = "gbtree",
                  maximize = TRUE, 
-                 print.every.n = 25,
+                 print.every.n = 50,
                  #early.stop.round = 25,
                  nrounds = 250,
                  eta = 0.01,
@@ -163,8 +241,8 @@ clf <- xgb.train(booster = "gbtree",
                  gamma= 0.05)
 
 
-prx2 <- predict(clf, x1d)
+prx3 <- predict(clf, x1d)
 
 # combine into the final forecast
 xfor <- data.frame(QuoteNumber = id_full, QuoteConversion_Flag = 0.5 * (prx1 + prx2))
-write_csv(xfor, path = "./submissions/ens2_20160101.csv")
+write_csv(xfor, path = "./submissions/ens_20160105.csv")
