5f4ebc28227f39345a5957357acd7e3cfd2c4ea8
mpearmain <michael.pearmain@gmail.com>
2016-01-29 14:27:39 +0000

    Fix of keras 3layers which gave leaderboard 0.963X
    Added more combinations and have different epoch lengths.  Also reports AUC at the end of each meta run to see how well it is approximatley doing

--- python/build_meta_3layer_keras.py
+++ python/build_meta_3layer_keras.py
@@ -15,18 +15,12 @@ import datetime
 projPath = './'
 dataset_version = "mp4"
 model_type = "keras"
-seed_value = 123
+seed_value = 578943
 todate = datetime.datetime.now().strftime("%Y%m%d")
 np.random.seed(seed_value)
 np.random.seed(1778)  # for reproducibility
 need_normalise=True
 need_categorical=False
-
-def save2model(submission,file_name,y_pre):
-    assert len(y_pre)==len(submission)
-    submission['QuoteConversion_Flag']=y_pre
-    submission.to_csv(file_name,index=False)
-    print ("saved files %s" % file_name)
 
 def getDummy(df,col):
     category_values=df[col].unique()
@@ -44,12 +38,9 @@ def getDummy(df,col):
 
     return df
 
-def generateFileName(model,params):
-     file_name="_".join([(key+"_"+ str(val))for key,val in params.items()])
-     return model+"_"+file_name+".csv"
-
 train = pd.read_csv(projPath + 'input/xtrain_'+ dataset_version + '.csv')
 id_train = train.QuoteNumber
+y_train_quote_flag = train.QuoteConversion_Flag
 y_train = train.QuoteConversion_Flag
 train.drop('QuoteNumber', axis = 1, inplace = True)
 train.drop('QuoteConversion_Flag', axis = 1, inplace = True)
@@ -70,26 +61,26 @@ for f in test.columns:#
 #try to encode all params less than 100 to be category
 if need_categorical:
     #row bind train and test
-    x=train.append(test,ignore_index=True)
+    x = train.append(test, ignore_index=True)
     del train
     del test
     for f in x.columns:#
         category_values= set(list(x[f].unique()))
-        if len(category_values)<4:
+        if len(category_values) < 4:
             print (f)
-            x=getDummy(x,f)
-    test = x.iloc[260753:,]
-    train = x.iloc[:260753:,]
+            x = getDummy(x, f)
+    test = x.iloc[260753:, ]
+    train = x.iloc[:260753:, ]
 
 encoder = LabelEncoder()
 y_train = encoder.fit_transform(y_train).astype(np.int32)
 y_train = np_utils.to_categorical(y_train)
 
 print ("processsing finished")
-# train = np.array(train)
-# train = train.astype(np.float32)
-# test = np.array(test)
-# test = test.astype(np.float32)
+train = np.array(train)
+train = train.astype(np.float32)
+test = np.array(test)
+test = test.astype(np.float32)
 if need_normalise:
     scaler = StandardScaler().fit(train)
     train = scaler.transform(train)
@@ -102,8 +93,8 @@ fold_index = xfolds.fold5
 fold_index = np.array(fold_index) - 1
 n_folds = len(np.unique(fold_index))
 
-nb_classes = y_train.shape[1]
-print(nb_classes, 'classes')
+nb_classes = 2
+print nb_classes, 'classes'
 
 dims = train.shape[1]
 print dims, 'dims'
@@ -111,17 +102,13 @@ print dims, 'dims'
 auc_scores=[]
 best_score=-1
 
-param_grid = [[1024, 0.1, 0.5, 1024, 0.5, 420, 0.5],
-              [1324, 0.15, 0.6, 512, 0.6, 520, 0.3]]
-epochs = 500
-
-xtrain = pd.DataFrame(train, index=id_train)
-xtest = pd.DataFrame(test, index=id_test)
-y_train = pd.DataFrame(y_train, index=id_train)
+param_grid = [[1024, 0.1, 0.5, 1024, 0.5, 42, 0.5, 400],
+              [1324, 0.15, 0.6, 512, 0.6, 520, 0.3, 500],
+              [666, 0.05, 0.4, 1512, 0.4, 874, 0.6, 200]]
 
 # storage structure for forecasts
-mvalid = np.zeros((xtrain.shape[0],len(param_grid)))
-mfull = np.zeros((xtest.shape[0],len(param_grid)))
+mvalid = np.zeros((train.shape[0],len(param_grid)))
+mfull = np.zeros((test.shape[0],len(param_grid)))
 
 ## build 2nd level forecasts
 for i in range(len(param_grid)):
@@ -150,20 +137,18 @@ for i in range(len(param_grid)):
             model.add(Activation('softmax'))
             model.compile(loss='binary_crossentropy', optimizer="sgd")
 
-        # loop over folds - Keeping as pandas for ease of use with xgb wrapper
-        for j in range(1 ,n_folds+1):
-            idx0 = xfolds[xfolds.fold5 != j].index
-            idx1 = xfolds[xfolds.fold5 == j].index
-            x0 = xtrain[xtrain.index.isin(idx0)]
-            x1 = xtrain[xtrain.index.isin(idx1)]
-            y0 = y_train[y_train.index.isin(idx0)]
-            y1 = y_train[y_train.index.isin(idx1)]
+            idx0 = np.where(fold_index != j)
+            idx1 = np.where(fold_index == j)
+            x0 = np.array(train)[idx0,:][0]
+            x1 = np.array(train)[idx1,:][0]
+            y0 = np.array(y_train)[idx0]
+            y1 = np.array(y_train)[idx1]
 
             # fit the model on observations associated with subject whichSubject in this fold
-            model.fit(x0, y0, nb_epoch=epochs, batch_size=1256)
+            model.fit(x0, y0, nb_epoch=x[7], batch_size=1256)
             mvalid[idx1,i] = model.predict_proba(x1)[:,0]
             y_pre = model.predict_proba(x1)[:,0]
-            scores = roc_auc_score(y1,y_pre)
+            scores = roc_auc_score(y1[:,0],y_pre)
             print 'AUC score', scores
             del model
             print "finished fold:", j
@@ -189,11 +174,9 @@ for i in range(len(param_grid)):
         model.add(Activation('softmax'))
         model.compile(loss='binary_crossentropy', optimizer="sgd")
         # fit on complete dataset
-        ytrain0 = np.zeros((xtrain.shape[0],2))
-        ytrain0[:,0] = y_train
-        ytrain0[:,1] = 1-y_train
-        model.fit(np.array(xtrain), ytrain0, nb_epoch=epochs, batch_size=1256)
-        mfull[:,i] = model.predict_proba(np.array(xtest))[:,0]
+
+        model.fit(np.array(train), y_train, nb_epoch=x[7], batch_size=1256)
+        mfull[:,i] = model.predict_proba(np.array(test))[:,0]
 
         del model
         print "finished full prediction"
@@ -203,11 +186,11 @@ for i in range(len(param_grid)):
 mvalid = pd.DataFrame(mvalid)
 mvalid.columns = [model_type + str(i) for i in range(0, mvalid.shape[1])]
 mvalid['QuoteNumber'] = id_train
-mvalid['QuoteConversion_Flag'] = y_train
+mvalid['QuoteConversion_Flag'] = y_train_quote_flag
 
 mfull = pd.DataFrame(mfull)
 mfull.columns = [model_type + str(i) for i in range(0, mfull.shape[1])]
-mfull['QuoteNumber'] = submission['QuoteNumber']
+mfull['QuoteNumber'] = id_test
 
 
 # save the files
